329c46429a67adf8a0ed32912114ed80e605a5e5
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-03-12 15:48:19 +0200

    1

--- Carsharing/Logic.py
+++ Carsharing/Logic.py
@@ -12,21 +12,24 @@ class Logic:
         price_per_hour = int(input('Цена за час в $: '))
         car = Cars(brand, model, price_per_hour)
         self.cars_price[car.get_model()] = car.get_price_per_hour()
-        self.cars[car.get_brand()] = car.get_model()
+        self.cars[car.get_model()] = car.get_brand()
 
     def take_a_car_to_car_sharing(self):
         brand = input('Марка: ')
         model = input('Модель: ')
-        for models in list(self.cars_price):
+        key_copy_cars_price = tuple(self.cars_price.keys())
+        key_copy_cars = tuple(self.cars.keys())
+        for models in key_copy_cars_price:
             if models == model:
                 print(f'Стоимость данной машины в час составит {self.cars_price[model]}$')
                 del self.cars_price[model]
+                del self.cars[model]
             else:
                 print('Машину не найдено. Введите существующую модель!')
 
     def list_of_available_cars(self):
         for car in self.cars:
-            print(f'{car},{self.cars[car]}')
+            print(f'{self.cars[car]},{car}')
 
     def price_information(self):
         car_price = []
@@ -37,7 +40,7 @@ class Logic:
             car_price.append(i)
 
         for cars in self.cars:
-            car.append(cars)
+            car.append(self.cars[cars])
 
         for i in range(len(self.cars_price)):
             print(f'{car[i - 1]} {car_price[i - 1]}/час')
