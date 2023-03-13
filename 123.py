ac3c02a54c09f88323d35f10da5aac1755cba3d4
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-03-13 15:40:59 +0200

    1

--- 123.py
+++ 123.py
@@ -1,7 +1,24 @@
+from Carsharing.Cars import Cars
+
+
 cars = [
     {'brand': 'BMW', 'model': 'M8', 'price': '100'},
     {'brand': 'Mers', 'model': 'G', 'price': '90'}
 ]
+
+brand1 = input('Марка: ')
+model1 = input('Модель: ')
+price_per_hour1 = int(input('Цена за час в $: '))
+
+car = Cars(brand1, model1, price_per_hour1)
+cars.append({'brand': brand1, 'model': model1, 'price': price_per_hour1})
+
+
+
+
+
+
+
 
 brand = input('Марка: ')
 model = input('Модель: ')
@@ -12,3 +29,5 @@ for car in cars:
         break
     else:
         print('Архип красавчик')
+
+
