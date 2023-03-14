e8b0f5442eca72391747a45c58fc27e0697efad3
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-03-14 17:21:11 +0200

    1

--- Carsharing/Menu.py
+++ Carsharing/Menu.py
@@ -9,13 +9,19 @@ log = Logic()
 
 
 def get_menu_choice():
-    choice = int(input('Какую бы оперцию вы хотли бы совершить:\n'
-                       '1. Добавить машину\n'
-                       '2. Взять машину в каршеринг\n'
-                       '3. Список доступных машин\n'
-                       '4. Выход\n'))
-
-    return choice
+    while True:
+        try:
+            choice = int(input('Какую бы операцию вы хотели бы совершить:\n'
+                               '1. Добавить машину\n'
+                               '2. Взять машину в каршеринг\n'
+                               '3. Список доступных машин\n'
+                               '4. Выход\n'))
+            if choice in [add_car, take_a_car_to_car_sharing, list_of_available_cars, quit]:
+                return choice
+            else:
+                print('Введите число от 1 до 4')
+        except ValueError:
+            print('Введите число от 1 до 4')
 
 
 def main():
@@ -37,4 +43,4 @@ def main():
             input('Для продолжения нажмите Enter')
 
         elif choice == quit:
-            exit()
+            break
