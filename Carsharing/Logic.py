0813fc59c926bde5afce5549c09060a37baee12a
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-03-28 19:54:26 +0300

    1

--- Carsharing/Logic.py
+++ Carsharing/Logic.py
@@ -1,28 +1,40 @@
 from Carsharing.Cars import Car
+import sqlite3
 
 
 class Logic:
+
     def __init__(self):
-        self.cars = []
+        self.conn = sqlite3.connect('carsharing.db')
+        self.cur = self.conn.cursor()
+        self.cur.execute('CREATE TABLE IF NOT EXISTS cars (brand TEXT, model TEXT, price_per_hour INTEGER)')
 
     def add_car(self):
         brand = input('Марка: ')
         model = input('Модель: ')
         price_per_hour = int(input('Цена за час в $: '))
         car = Car(brand, model, price_per_hour)
-        self.cars.append(car)
+        self.cur.execute('INSERT INTO cars VALUES (?, ?, ?)', (brand, model, price_per_hour))
+        self.conn.commit()
 
     def take_a_car_to_car_sharing(self):
         brand = input('Марка: ')
         model = input('Модель: ')
-        for car in self.cars:
-            if car.brand == brand and car.model == model:
-                print(f"Стоимость данной машины в час составит {car.price_per_hour}$")
-                self.cars.remove(car)
-                break
+        self.cur.execute('SELECT * FROM cars WHERE brand = ? AND model = ?', (brand, model))
+        row = self.cur.fetchone()
+        if row is not None:
+            car = Car(*row)
+            print(f"Стоимость данной машины в час составит {car.price_per_hour}$")
+            self.cur.execute('DELETE FROM cars WHERE brand = ? AND model = ?', (brand, model))
+            self.conn.commit()
         else:
             print('Машину не найдено. Введите существующую модель!')
 
     def list_of_available_cars(self):
-        for car in self.cars:
-            print(f"{car.brand}, {car.model} : {car.price_per_hour}$/час")
+        self.cur.execute('SELECT * FROM cars')
+        rows = self.cur.fetchall()
+        if len(rows) == 0:
+            print('Список доступных машин пуст')
+        else:
+            for row in rows:
+                print(f"{row[0]}, {row[1]} : {row[2]}$/час")
